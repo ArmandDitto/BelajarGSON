0192accfffa745784a07c725276377d29158e944
Muhamad Armanditto <deepfreeze1197@gmail.com>
2020-02-13 09:39:15 +0700

    Membuat File people.json

--- app/src/main/java/com/ditto/training/belajargson/MainActivity.java
+++ app/src/main/java/com/ditto/training/belajargson/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Gson gsonku = new Gson();
+        People peopleku = new People("Rohman", 18, "gmail@rohman.com");
+        String jsonku = gsonku.toJson(peopleku);
     }
 }
---
+++ app/src/main/java/com/ditto/training/belajargson/People.java
@@ -0,0 +1,15 @@
+package com.ditto.training.belajargson;
+
+public class People {
+    private String peopleName;
+    private int peopleAge;
+    private String peopleMail;
+
+    public People(String peopleName, int peopleAge, String peopleMail) {
+        this.peopleName = peopleName;
+        this.peopleAge = peopleAge;
+        this.peopleMail = peopleMail;
+    }
+
+
+}
