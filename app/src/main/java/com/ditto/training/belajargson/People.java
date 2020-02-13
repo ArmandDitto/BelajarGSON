eee3dc9c83de7bbef8a9b2465ded0624bc3db1e8
Muhamad Armanditto <deepfreeze1197@gmail.com>
2020-02-13 11:36:30 +0700

    Melakukan Deserialisasi JSON

--- app/src/main/java/com/ditto/training/belajargson/People.java
+++ app/src/main/java/com/ditto/training/belajargson/People.java
@@ -1,14 +1,19 @@
 package com.ditto.training.belajargson;
 
+import com.google.gson.annotations.SerializedName;
+
 public class People {
+    @SerializedName("people_name")
     private String peopleName;
+    @SerializedName("people_age")
     private int peopleAge;
+    @SerializedName("people_mail")
     private String peopleMail;
 
-    public People(String peopleName, int peopleAge, String peopleMail) {
-        this.peopleName = peopleName;
-        this.peopleAge = peopleAge;
-        this.peopleMail = peopleMail;
+    public People(String peopleNameku, int peopleAgeku, String peopleMailku) {
+        peopleName = peopleNameku;
+        peopleAge = peopleAgeku;
+        peopleMail = peopleMailku;
     }
 
 
