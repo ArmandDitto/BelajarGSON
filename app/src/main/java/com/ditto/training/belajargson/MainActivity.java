989c55a0fb7d503dc0ea3b2776258ff90cc3cc5a
Muhamad Armanditto <deepfreeze1197@gmail.com>
2020-02-13 12:13:42 +0700

    Melakukan Deserialisasi JSON dengan tambahan Address

--- app/src/main/java/com/ditto/training/belajargson/MainActivity.java
+++ app/src/main/java/com/ditto/training/belajargson/MainActivity.java
@@ -7,18 +7,16 @@ import android.os.Bundle;
 import com.google.gson.Gson;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Gson gsonku = new Gson();
         People.Address addressku = new People.Address("Rumah", "Griya Ciledug", "Tangerang");
-        People peopleku = new People("Rohman", 18, "gmail@rohman.com", addressku);
-        String jsonku = gsonku.toJson(peopleku);
+        /*People peopleku = new People("Rohman", 18, "gmail@rohman.com", addressku);
+        String jsonku = gsonku.toJson(peopleku);*/
 
-        /*String jsonku = "{\"people_age\":18,\"people_mail\":\"gmail@rohman.com\",\"people_name\":\"Rohman\"}";
-        People peopleku = gsonku.fromJson(jsonku, People.class);*/
+        String jsonku = "{\"people_address\":{\"address_city\":\"Tangerang\",\"address_detail\":\"Griya Ciledug\",\"address_name\":\"Rumah\"},\"people_age\":18,\"people_mail\":\"gmail@rohman.com\",\"people_name\":\"Rohman\"}";
+        People peopleku = gsonku.fromJson(jsonku, People.class);
     }
 }
